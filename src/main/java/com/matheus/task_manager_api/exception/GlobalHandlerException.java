d3dd41c14c7fb28ae8cf5751d82a1c7ee518a4b1
Math713 <matheusrr3004@gmail.com>
2026-04-20 12:05:44 -0300

    feat: add handler to BadCredentialsException

--- src/main/java/com/matheus/task_manager_api/exception/GlobalHandlerException.java
+++ src/main/java/com/matheus/task_manager_api/exception/GlobalHandlerException.java
@@ -3,6 +3,7 @@ package com.matheus.task_manager_api.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -127,6 +128,18 @@ public class GlobalHandlerException {
         );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentials(BadCredentialsException exception,
+                                                         HttpServletRequest request) {
+        ApiError body = new ApiError(
+                401,
+                "INVALID_CREDENTIALS",
+                "Invalid email or password",
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
     }
 
     @ExceptionHandler(Exception.class)
